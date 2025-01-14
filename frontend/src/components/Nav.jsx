ea858fe5cd039fa1b6ef895021161621ddbb66c2
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2025-01-14 23:15:15 +0300

    feat: update Nav component to include navigation links with react-router-dom

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Nav() {
   return (
     <header>
         <nav>
-            
-        </nav>
+            <ul className='flex flex-col'>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/apps'>Apps</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
+            </ul>
+        </nav> 
     </header>
   )
 }
