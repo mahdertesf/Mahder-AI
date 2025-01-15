102d0589805fbdbcf44e5077775130bb94a369df
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2025-01-16 01:42:35 +0300

    feat: integrate Hero component into Home page for improved user experience

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 function Home() {
   return (
-    <div className='h-screen'>Home</div>
+    <main className='flex-1'>
+      <Hero/>
+    </main>
   )
 }
 
