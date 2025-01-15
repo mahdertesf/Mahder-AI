1035ab29aa2a8a650d6064e2f8cf9052039d438c
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2025-01-16 01:42:10 +0300

    style: adjust BigNav component header positioning for improved layout

--- frontend/src/components/BigNav.jsx
+++ frontend/src/components/BigNav.jsx
@@ -1,18 +1,13 @@
 import { React, useState } from "react";
 import { Link } from "react-router-dom";
-import { AiOutlineHome } from "react-icons/ai";
-import { RiApps2Line } from "react-icons/ri";
-import { RxQuestionMarkCircled } from "react-icons/rx";
-import { MdOutlineEmail } from "react-icons/md";
-import { IoChevronBackCircleSharp } from "react-icons/io5";
-import Logo from "../assets/images/logo.png";
+
 import MobileNav from "./MobileNav";
 import InsideNav from "./InsideNav";
 
 function BigNav(props) {
   return (
     <>
-      <header className="h-screen w-auto bg-white z-50 max-md:hidden">
+      <header className=" bottom-0  w-auto bg-white z-50 max-md:hidden ">
       <InsideNav nav_elements={props.nav_elements} navHandler={props.navHandler}  />
         
       </header>
