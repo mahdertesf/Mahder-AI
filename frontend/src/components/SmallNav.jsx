5d0a6317226f2407a2a06f47431b41a506c36abd
Mahder Tesfaye Abebe <mahder.tesfaye@aait.edu.et>
2025-01-15 14:53:07 +0300

    feat: integrate MobileNav into SmallNav for enhanced navigation options

--- frontend/src/components/SmallNav.jsx
+++ frontend/src/components/SmallNav.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { FaCircleChevronRight } from "react-icons/fa6";
 import {Link} from 'react-router-dom'
+import MobileNav from "./MobileNav";
+
 function SmallNav(props) {
-  return (
-    <header className="h-screen w-[5%] bg-white z-50 transition-transform duration-300 ">
+  return (<>
+  <header className="h-screen w-[5%] bg-white z-50 transition-transform duration-300 max-md:hidden">
     <nav >
       <div className="flex flex-col items-center justify-between  border-b-2 py-4 ">
         <img src={props.logo} alt="logo" className="w-16 h-16" />
-        <FaCircleChevronRight className="text-2xl " onClick={props.navHandler} />
+        <Link><FaCircleChevronRight className="text-2xl " onClick={props.navHandler} /></Link>
       </div>
-      <ul className="flex flex-col pt-7 pl-7 gap-5">
+      <ul className="flex flex-col items-center justify-center pt-7 mx-6 gap-5">
         {props.nav_elements.map((element) => {
           return (
             <li key={element.name} className="flex items-center gap-3">
@@ -20,6 +22,9 @@ function SmallNav(props) {
       </ul>
     </nav>
     </header>
+    <MobileNav nav_elements={props.nav_elements} navHandler={props.navHandler}  />
+  </>
+    
   );
 }
 
