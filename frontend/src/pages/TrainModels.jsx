16982ad85155d32cd35992819be66b20c36a1276
Mahder Tesfaye Abebe <mahdertesfaye11@gmail.com>
2025-03-20 23:12:22 +0300

    feat: add TrainModels component for model fine-tuning interface

---
+++ frontend/src/pages/TrainModels.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function TrainModels() {
+  return (
+    <main className='h-min-screen'>
+        <div>
+            <h1>Fine tune Gemma models of your choice without any code and download your checkpoints</h1>
+        </div>
+        <div>
+            <div>
+
+            </div>
+            <div>
+                
+            </div>
+
+        </div>
+    </main>
+  )
+}
+
+export default TrainModels
