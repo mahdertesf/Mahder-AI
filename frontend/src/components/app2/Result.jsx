c42c8edf23b910aa395038d42a34e15e6dccbd80
Mahder Tesfaye Abebe <mahdertesfaye11@gmail.com>
2025-03-25 02:58:13 +0300

    feat: add Result component for downloading checkpoints

---
+++ frontend/src/components/app2/Result.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Result() {
+  return (
+    <div>
+        <button className='bg-yellow-400 p-3 rounded-full'>Download Checkpoint</button>
+    </div>
+  )
+}
+
+export default Result
